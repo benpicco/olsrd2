3b5648b8ed4237ff8353cf2a0e0c07f6b6255b63
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2012-01-13 13:04:07 +0100

    Change OLSRD variable names in makefiles to OON (olsr.org network framework)

--- lib/plugin.cmake
+++ lib/plugin.cmake
@@ -2,13 +2,13 @@
 ADD_DEFINITIONS(-DPLUGIN_FULLNAME=${libname})
 
 ADD_LIBRARY(static_${libname} STATIC ${source})
-ADD_LIBRARY(olsrd_${libname} SHARED ${source})
+ADD_LIBRARY(${OONF_LIBPREFIX}_${libname} SHARED ${source})
 
 IF(WIN32)
-    TARGET_LINK_LIBRARIES(olsrd_${libname} ws2_32 iphlpapi olsrd)
+    TARGET_LINK_LIBRARIES(${OONF_LIBPREFIX}_${libname} ws2_32 iphlpapi ${OONF_APP})
 ENDIF(WIN32)
 
-SET_TARGET_PROPERTIES(olsrd_${libname} PROPERTIES SOVERSION ${OLSRD_VERSION})
+SET_TARGET_PROPERTIES(${OONF_LIBPREFIX}_${libname} PROPERTIES SOVERSION ${OONF_VERSION})
 
 UNSET(source)
 UNSET(libname)
