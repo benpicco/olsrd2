b2eb98952f7e1d5a6b6bb52990dbc057f0249e71
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2012-01-13 14:04:12 +0100

    Fix build system in builddata

--- src/builddata/generate_builddata.cmake
+++ src/builddata/generate_builddata.cmake
@@ -5,14 +5,14 @@ find_program(found_git git)
 
 IF(${found_git} STREQUAL "found_git-NOTFOUND" OR NOT EXISTS ${GIT})
     # git executable or repository (.git) is not available
-    SET(OLSRD_SRC_GIT "cannot read git repository")
-    SET(OLSRD_SRC_CHANGE "")
+    SET(OONF_SRC_GIT "cannot read git repository")
+    SET(OONF_SRC_CHANGE "")
 ELSE()
     # everything is fine, read commit and diff stat
     execute_process(COMMAND git describe --tags 
-        OUTPUT_VARIABLE OLSRD_SRC_GIT OUTPUT_STRIP_TRAILING_WHITESPACE)
+        OUTPUT_VARIABLE OONF_SRC_GIT OUTPUT_STRIP_TRAILING_WHITESPACE)
     execute_process(COMMAND git diff --shortstat HEAD ./src/ ./lib 
-        OUTPUT_VARIABLE OLSRD_SRC_CHANGE OUTPUT_STRIP_TRAILING_WHITESPACE)
+        OUTPUT_VARIABLE OONF_SRC_CHANGE OUTPUT_STRIP_TRAILING_WHITESPACE)
 ENDIF()
 
 # create builddata file
