6f4bb7bc40392c8cd1d36ba38570574fdec8e308
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2013-01-15 15:09:04 +0100

    Add NHDP-CHECK plugin

---
+++ src-plugins/nhdpcheck/nhdpcheck.c
@@ -0,0 +1,365 @@
+
+/*
+ * The olsr.org Optimized Link-State Routing daemon(olsrd)
+ * Copyright (c) 2004-2012, the olsr.org team - see HISTORY file
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ * * Neither the name of olsr.org, olsrd nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Visit http://www.olsr.org for more information.
+ *
+ * If you find this software useful feel free to make a donation
+ * to the project. For more information see the website or contact
+ * the copyright holders.
+ *
+ */
+
+#include "common/common_types.h"
+#include "common/autobuf.h"
+
+#include "core/olsr_logging.h"
+#include "core/olsr_plugins.h"
+#include "rfc5444/rfc5444_iana.h"
+#include "rfc5444/rfc5444_conversion.h"
+#include "rfc5444/rfc5444_reader.h"
+#include "tools/olsr_rfc5444.h"
+#include "tools/olsr_cfg.h"
+
+#include "nhdp/nhdp_interfaces.h"
+
+/* prototypes */
+static int _cb_plugin_load(void);
+static int _cb_plugin_unload(void);
+static int _cb_plugin_enable(void);
+static int _cb_plugin_disable(void);
+
+/* plugin declaration */
+OLSR_PLUGIN7 {
+  .descr = "OLSRD2 nhdpcheck plugin",
+  .author = "Henning Rogge",
+
+  .load = _cb_plugin_load,
+  .unload = _cb_plugin_unload,
+  .enable = _cb_plugin_enable,
+  .disable = _cb_plugin_disable,
+
+  .deactivate = true,
+};
+
+/* NHDP message TLV array index */
+enum {
+  IDX_TLV_ITIME,
+  IDX_TLV_VTIME,
+};
+
+/* NHDP address TLV array index pass 1 */
+enum {
+  IDX_ADDRTLV_LOCAL_IF,
+  IDX_ADDRTLV_LINK_STATUS,
+  IDX_ADDRTLV_OTHER_NEIGHB,
+};
+
+/* prototypes */
+static enum rfc5444_result _cb_message_start_callback(struct rfc5444_reader_tlvblock_consumer *,
+    struct rfc5444_reader_tlvblock_context *context);
+static enum rfc5444_result _cb_messagetlvs(struct rfc5444_reader_tlvblock_consumer *consumer,
+      struct rfc5444_reader_tlvblock_context *context);
+static enum rfc5444_result _cb_addresstlvs(struct rfc5444_reader_tlvblock_consumer *consumer,
+      struct rfc5444_reader_tlvblock_context *context);
+
+/* definition of the RFC5444 reader components */
+static struct rfc5444_reader_tlvblock_consumer _nhdp_message_consumer = {
+  .start_callback = _cb_message_start_callback,
+  .block_callback = _cb_messagetlvs,
+};
+
+static struct rfc5444_reader_tlvblock_consumer_entry _nhdp_message_tlvs[] = {
+  [IDX_TLV_ITIME] = { .type = RFC5444_MSGTLV_INTERVAL_TIME },
+  [IDX_TLV_VTIME] = { .type = RFC5444_MSGTLV_VALIDITY_TIME },
+};
+
+static struct rfc5444_reader_tlvblock_consumer _nhdp_address_consumer = {
+  .block_callback = _cb_addresstlvs,
+};
+
+static struct rfc5444_reader_tlvblock_consumer_entry _nhdp_address_tlvs[] = {
+  [IDX_ADDRTLV_LOCAL_IF] = { .type = RFC5444_ADDRTLV_LOCAL_IF },
+  [IDX_ADDRTLV_LINK_STATUS] = { .type = RFC5444_ADDRTLV_LINK_STATUS },
+  [IDX_ADDRTLV_OTHER_NEIGHB] = { .type = RFC5444_ADDRTLV_OTHER_NEIGHB },
+};
+
+/* nhdp multiplexer/protocol */
+static struct olsr_rfc5444_protocol *_protocol = NULL;
+
+static enum log_source LOG_NHDP_CHECK = LOG_MAIN;
+
+
+/**
+ * Constructor of plugin
+ * @return 0 if initialization was successful, -1 otherwise
+ */
+static int
+_cb_plugin_load(void) {
+  return 0;
+}
+
+/**
+ * Destructor of plugin
+ * @return always returns 0 (cannot fail)
+ */
+static int
+_cb_plugin_unload(void) {
+  return 0;
+}
+
+/**
+ * Enable plugin
+ * @return always returns 0 (cannot fail)
+ */
+static int
+_cb_plugin_enable(void) {
+  _protocol = olsr_rfc5444_add_protocol(RFC5444_PROTOCOL, true);
+  if (_protocol == NULL) {
+    return -1;
+  }
+
+  LOG_NHDP_CHECK = olsr_log_register_source("nhdp_check");
+
+  rfc5444_reader_add_message_consumer(
+      &_protocol->reader, &_nhdp_message_consumer,
+      _nhdp_message_tlvs, ARRAYSIZE(_nhdp_message_tlvs),
+      RFC5444_MSGTYPE_HELLO, RFC5444_VALIDATOR_PRIORITY);
+  rfc5444_reader_add_address_consumer(
+      &_protocol->reader, &_nhdp_address_consumer,
+      _nhdp_address_tlvs, ARRAYSIZE(_nhdp_address_tlvs),
+      RFC5444_MSGTYPE_HELLO, RFC5444_VALIDATOR_PRIORITY);
+
+  return 0;
+}
+
+/**
+ * Disable plugin
+ * @return always returns 0 (cannot fail)
+ */
+static int
+_cb_plugin_disable(void) {
+  rfc5444_reader_remove_message_consumer(
+      &_protocol->reader, &_nhdp_message_consumer);
+  rfc5444_reader_remove_address_consumer(
+      &_protocol->reader, &_nhdp_address_consumer);
+  return 0;
+}
+
+static enum rfc5444_result
+_cb_message_start_callback(struct rfc5444_reader_tlvblock_consumer *consumer __attribute__((unused)),
+    struct rfc5444_reader_tlvblock_context *context) {
+  struct nhdp_interface *interf;
+
+  interf = nhdp_interface_get(_protocol->input_interface->name);
+  assert(interf);
+
+  /* check address length */
+  if ((context->addr_len == 16 && interf->mode == NHDP_IPV4)
+      || (context->addr_len == 4 && interf->mode == NHDP_IPV6)
+      || (context->addr_len != 4 && context->addr_len != 16)) {
+    OLSR_INFO(LOG_NHDP_CHECK,
+        "Dropped NHDP message with addrlen %d on interface %s (%s)",
+        context->addr_len,
+        nhdp_interface_get_name(interf), NHDP_INTERFACE_MODES[interf->mode]);
+    return RFC5444_DROP_MESSAGE;
+  }
+
+  /* drop if message has hoplimit and its not 1 */
+  if (context->has_hoplimit && context->hoplimit != 1) {
+    OLSR_INFO(LOG_NHDP_CHECK,
+            "Dropped NHDP message with hoplimit %d", context->hoplimit);
+    return RFC5444_DROP_MESSAGE;
+  }
+
+  /* drop if message has hopcount and its not 0 */
+  if (context->has_hopcount && context->hopcount != 0) {
+    OLSR_INFO(LOG_NHDP_CHECK,
+            "Dropped NHDP message with hopcount %d", context->hopcount);
+    return RFC5444_DROP_MESSAGE;
+  }
+
+  return RFC5444_OKAY;
+}
+
+static enum rfc5444_result
+_cb_messagetlvs(struct rfc5444_reader_tlvblock_consumer *consumer __attribute__((unused)),
+      struct rfc5444_reader_tlvblock_context *context __attribute__((unused))) {
+  /* drop message if it has no VTIME TLV or has more than one */
+  if (_nhdp_message_tlvs[IDX_TLV_VTIME].tlv == NULL
+      || _nhdp_message_tlvs[IDX_TLV_VTIME].duplicate_tlv) {
+    OLSR_INFO(LOG_NHDP_CHECK,
+            "Dropped NHDP message with no or multiple VTIME TLVs");
+    return RFC5444_DROP_MESSAGE;
+  }
+
+  /* check if VTIME TLV has length 1 */
+  if (_nhdp_message_tlvs[IDX_TLV_VTIME].tlv->length != 1) {
+    OLSR_INFO(LOG_NHDP_CHECK,
+            "Dropped NHDP message with VTIME TLV length %d",
+            _nhdp_message_tlvs[IDX_TLV_VTIME].tlv->length);
+    return RFC5444_DROP_MESSAGE;
+  }
+
+  if (_nhdp_message_tlvs[IDX_TLV_ITIME].tlv) {
+    /* check if message has multiple ITIME TLVs */
+    if (_nhdp_message_tlvs[IDX_TLV_ITIME].duplicate_tlv) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+                  "Dropped NHDP message with multiple ITIME TLVs");
+      return RFC5444_DROP_MESSAGE;
+    }
+    if (_nhdp_message_tlvs[IDX_TLV_ITIME].tlv->length != 1) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+                  "Dropped NHDP message with ITIME TLV length %d",
+                  _nhdp_message_tlvs[IDX_TLV_ITIME].tlv->length);
+      return RFC5444_DROP_MESSAGE;
+    }
+  }
+  return RFC5444_OKAY;
+}
+
+static enum rfc5444_result
+_cb_addresstlvs(struct rfc5444_reader_tlvblock_consumer *consumer __attribute__((unused)),
+    struct rfc5444_reader_tlvblock_context *context __attribute__((unused))) {
+  struct netaddr_str buf;
+  struct netaddr addr;
+  uint8_t value;
+
+  if (netaddr_from_binary(&addr, context->addr, context->addr_len, 0) != 0
+      || netaddr_to_string(&buf, &addr) != 0) {
+    OLSR_WARN(LOG_NHDP_CHECK, "Error converting address into netaddr");
+    return RFC5444_DROP_MESSAGE;
+  }
+
+  if (_nhdp_address_tlvs[IDX_ADDRTLV_LOCAL_IF].tlv != NULL) {
+    /* check for duplicate LOCAL_IF TLV */
+    if (_nhdp_address_tlvs[IDX_ADDRTLV_LOCAL_IF].duplicate_tlv) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+          "Dropped NHDP message, address %s had multiple LOCAL_IF TLVs",
+          buf.buf);
+      return RFC5444_DROP_MESSAGE;
+    }
+
+    /* check for bad length of LOCAL_IF TLV */
+    if (_nhdp_address_tlvs[IDX_ADDRTLV_LOCAL_IF].tlv->length != 1) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+          "Dropped NHDP message, address %s had LOCAL_IF TLV length %d",
+          buf.buf, _nhdp_address_tlvs[IDX_ADDRTLV_LOCAL_IF].tlv->length);
+      return RFC5444_DROP_MESSAGE;
+    }
+
+    /* check for bad value of LOCAL_IF TLV */
+    value = _nhdp_address_tlvs[IDX_ADDRTLV_LOCAL_IF].tlv->single_value[0];
+    if (value != RFC5444_LOCALIF_THIS_IF
+        && value != RFC5444_LOCALIF_OTHER_IF) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+          "Dropped NHDP message, address %s had LOCAL_IF TLV value %u",
+          buf.buf, (uint32_t)value);
+      return RFC5444_DROP_MESSAGE;
+    }
+
+    /* check if address had both LOCAL_IF and LINK_STATUS TLV */
+    if (_nhdp_address_tlvs[IDX_ADDRTLV_LINK_STATUS].tlv != NULL) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+          "Dropped NHDP message, address %s had LOCAL_IF and LINK_STATUS TLV",
+          buf.buf);
+      return RFC5444_DROP_MESSAGE;
+    }
+
+    /* check if address had both LOCAL_IF and OTHER_NEIGH TLV */
+    if (_nhdp_address_tlvs[IDX_ADDRTLV_OTHER_NEIGHB].tlv != NULL) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+          "Dropped NHDP message, address %s had LOCAL_IF and OTHER_NEIGHB TLV",
+          buf.buf);
+      return RFC5444_DROP_MESSAGE;
+    }
+  }
+
+  if (_nhdp_address_tlvs[IDX_ADDRTLV_LINK_STATUS].tlv != NULL) {
+    /* check for duplicate LINK_STATUS TLV */
+    if (_nhdp_address_tlvs[IDX_ADDRTLV_LINK_STATUS].duplicate_tlv) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+          "Dropped NHDP message, address %s had multiple LINK_STATUS TLVs",
+          buf.buf);
+      return RFC5444_DROP_MESSAGE;
+    }
+
+    /* check for bad length of LINK_STATUS TLV */
+    if (_nhdp_address_tlvs[IDX_ADDRTLV_LINK_STATUS].tlv->length != 1) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+          "Dropped NHDP message, address %s had LINK_STATUS TLV length %d",
+          buf.buf, _nhdp_address_tlvs[IDX_ADDRTLV_LINK_STATUS].tlv->length);
+      return RFC5444_DROP_MESSAGE;
+    }
+
+    /* check for bad value of LINK_STATUS TLV */
+    value = _nhdp_address_tlvs[IDX_ADDRTLV_LINK_STATUS].tlv->single_value[0];
+    if (value != RFC5444_LINKSTATUS_LOST
+        && value != RFC5444_LINKSTATUS_SYMMETRIC
+        && value != RFC5444_LINKSTATUS_HEARD) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+          "Dropped NHDP message, address %s had LINK_STATUS TLV value %u",
+          buf.buf, (uint32_t)value);
+      return RFC5444_DROP_MESSAGE;
+    }
+  }
+
+  if (_nhdp_address_tlvs[IDX_ADDRTLV_OTHER_NEIGHB].tlv != NULL) {
+    /* check for duplicate OTHER_NEIGH TLV */
+    if (_nhdp_address_tlvs[IDX_ADDRTLV_OTHER_NEIGHB].duplicate_tlv) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+          "Dropped NHDP message, address %s had multiple OTHER_NEIGHB TLVs",
+          buf.buf);
+      return RFC5444_DROP_MESSAGE;
+    }
+
+    /* check for bad length of OTHER_NEIGH TLV */
+    if (_nhdp_address_tlvs[IDX_ADDRTLV_OTHER_NEIGHB].tlv->length != 1) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+          "Dropped NHDP message, address %s had OTHER_NEIGH TLV length %d",
+          buf.buf, _nhdp_address_tlvs[IDX_ADDRTLV_OTHER_NEIGHB].tlv->length);
+      return RFC5444_DROP_MESSAGE;
+    }
+
+    /* check for bad value of OTHER_NEIGH TLV */
+    value = _nhdp_address_tlvs[IDX_ADDRTLV_OTHER_NEIGHB].tlv->single_value[0];
+    if (value != RFC5444_OTHERNEIGHB_SYMMETRIC
+        && value != RFC5444_OTHERNEIGHB_LOST) {
+      OLSR_INFO(LOG_NHDP_CHECK,
+          "Dropped NHDP message, address %s had OTHER_NEIGH TLV value %u",
+          buf.buf, (uint32_t)value);
+      return RFC5444_DROP_MESSAGE;
+    }
+  }
+
+  return RFC5444_OKAY;
+}
