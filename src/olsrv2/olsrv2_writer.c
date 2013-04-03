064b1d31b59b8afd57b37e8592215bd7ff261d30
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2013-04-03 14:21:17 +0200

    More code for olsrv2

--- src/olsrv2/olsrv2_writer.c
+++ src/olsrv2/olsrv2_writer.c
@@ -40,20 +40,109 @@
  */
 
 #include "common/common_types.h"
+#include "rfc5444/rfc5444_iana.h"
+#include "rfc5444/rfc5444.h"
+#include "rfc5444/rfc5444_writer.h"
+#include "core/olsr_logging.h"
 #include "tools/olsr_rfc5444.h"
 
+#include "olsrv2/olsrv2.h"
 #include "olsrv2/olsrv2_writer.h"
 
-struct olsr_rfc5444_protocol *_protocol;
+/* constants */
+enum {
+  IDX_ADDRTLV_NBR_ADDR_TYPE,
+  IDX_ADDRTLV_GATEWAY,
+};
+
+/* Prototypes */
+static void _cb_addMessageHeader(
+    struct rfc5444_writer *, struct rfc5444_writer_message *);
+static void _cb_addMessageTLVs(
+    struct rfc5444_writer *, struct rfc5444_writer_content_provider *);
+static void _cb_addAddresses(
+    struct rfc5444_writer *, struct rfc5444_writer_content_provider *);
+
+/* definition of NHDP writer */
+static struct rfc5444_writer_message *_olsrv2_message = NULL;
+
+static struct rfc5444_writer_content_provider _olsrv2_msgcontent_provider = {
+  .msg_type = RFC5444_MSGTYPE_TC,
+  .addMessageTLVs = _cb_addMessageTLVs,
+  .addAddresses = _cb_addAddresses,
+};
+
+static struct rfc5444_writer_tlvtype _olsrv2_addrtlvs[] = {
+  [IDX_ADDRTLV_NBR_ADDR_TYPE] =  { .type = RFC5444_ADDRTLV_NBR_ADDR_TYPE },
+  [IDX_ADDRTLV_GATEWAY]       =  { .type = RFC5444_ADDRTLV_GATEWAY },
+};
+
+static struct olsr_rfc5444_protocol *_protocol;
+
+static enum log_source LOG_OLSRV2_W = LOG_MAIN;
 
 int
 olsrv2_writer_init(struct olsr_rfc5444_protocol *protocol) {
   _protocol = protocol;
 
+  LOG_OLSRV2_W = olsr_log_register_source("olsrv2_w");
+
+  _olsrv2_message = rfc5444_writer_register_message(
+      &_protocol->writer, RFC5444_MSGTYPE_HELLO, true, 4);
+  if (_olsrv2_message == NULL) {
+    OLSR_WARN(LOG_OLSRV2, "Could not register OLSRv2 TC message");
+    return -1;
+  }
+
+  _olsrv2_message->addMessageHeader = _cb_addMessageHeader;
+
+  if (rfc5444_writer_register_msgcontentprovider(
+      &_protocol->writer, &_olsrv2_msgcontent_provider,
+      _olsrv2_addrtlvs, ARRAYSIZE(_olsrv2_addrtlvs))) {
+
+    OLSR_WARN(LOG_OLSRV2, "Count not register OLSRv2 msg contentprovider");
+    rfc5444_writer_unregister_message(&_protocol->writer, _olsrv2_message);
+    return -1;
+  }
   return 0;
 }
 
 void
 olsrv2_writer_cleanup(void) {
+  /* remove pbb writer */
+  rfc5444_writer_unregister_content_provider(
+      &_protocol->writer, &_olsrv2_msgcontent_provider,
+      _olsrv2_addrtlvs, ARRAYSIZE(_olsrv2_addrtlvs));
+  rfc5444_writer_unregister_message(&_protocol->writer, _olsrv2_message);
+}
+
+static void
+_cb_addMessageHeader(struct rfc5444_writer *writer,
+    struct rfc5444_writer_message *message) {
+  const struct netaddr *orig;
+
+  orig = olsrv2_get_originator();
+  if (netaddr_get_address_family(orig) == AF_INET) {
+    rfc5444_writer_set_msg_addrlen(writer, message, 4);
+  }
+  else  {
+    rfc5444_writer_set_msg_addrlen(writer, message, 6);
+  }
+  rfc5444_writer_set_msg_header(writer, message, true, true, true, true);
+
+  OLSR_DEBUG(LOG_OLSRV2_W, "Generate TC");
+}
+
+static void
+_cb_addMessageTLVs(struct rfc5444_writer *writer,
+    struct rfc5444_writer_content_provider *prv) {
+  uint8_t vtime_encoded, itime_encoded;
+
+
+}
+
+static void
+_cb_addAddresses(struct rfc5444_writer *writer,
+    struct rfc5444_writer_content_provider *prv) {
 
 }
