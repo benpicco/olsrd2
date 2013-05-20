fdd23a04a48c27f79f39e379dc799b8c1ff07bb9
Henning Rogge <hrogge@googlemail.com>
2013-05-20 12:55:55 +0200

    Better output for licence checking script

--- files/check_licences.sh
+++ files/check_licences.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 
-LEN=`cat default_licence.txt |wc -c`
+LEN=`cat ./files/default_licence.txt |wc -c`
 
-for file in $(eval find ../src*  -type f -name *[.][ch] $EXCEPT)
+OKAY=0
+BAD=0
+
+filecount=`echo $files|wc -l`
+
+for file in $(eval find ./src* -type f -name *[.][ch] $EXCEPT)
 do
-  cmp --bytes $LEN $file default_licence.txt
-  if [ $? != 0 ]
-  then
-    # do nothing
-    true
-  fi
+	cmp --bytes $LEN $file ./files/default_licence.txt
+	if [ $? != 0 ]
+	then
+		BAD=$((${BAD} + 1))
+	else
+		OKAY=$((${OKAY} + 1))
+	fi
 done
+
+if [ ${OKAY} != 0 ]
+then
+	echo "Found ${OKAY} source/header files with the correct header"
+elif [ ${BAD} != 0 ]
+then
+	echo "Found ${BAD} source/header files with the wrong or an outdated header"
+else
+	echo "No files found, please run script from the main directory of the repository"
+fi
