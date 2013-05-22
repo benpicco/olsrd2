f5fbc97487f2654e8832308fa31fb75edab9e667
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2013-05-22 10:16:44 +0200

    Automatically generate app-version from git tag

--- cmake/generate_builddata.cmake
+++ cmake/generate_builddata.cmake
@@ -3,14 +3,21 @@
 # look for git executable 
 find_program(found_git git)
 
-IF(${found_git} STREQUAL "found_git-NOTFOUND" OR NOT EXISTS ${GIT})
-    # git executable or repository (.git) is not available
-    SET(OONF_SRC_GIT "cannot read git repository")
-ELSE()
-    # everything is fine, read commit and diff stat
-    execute_process(COMMAND git describe --always --long --tags --dirty --match "v[0-9]*"
-        OUTPUT_VARIABLE OONF_SRC_GIT OUTPUT_STRIP_TRAILING_WHITESPACE)
+SET(OONF_APP_GIT "cannot read git repository")
+
+IF(NOT ${found_git} STREQUAL "found_git-NOTFOUND")
+	# get git description WITH dirty flag
+	execute_process(COMMAND git describe --always --long --tags --dirty --match "v[0-9]*"
+		OUTPUT_VARIABLE OONF_APP_GIT OUTPUT_STRIP_TRAILING_WHITESPACE)
 ENDIF()
+
+# compare with version number
+STRING(REGEX MATCH "v${OONF_APP_VERSION}-" FOUND_VERSION ${OONF_APP_GIT})
+IF (NOT FOUND_VERSION)
+	message (FATAL_ERROR "App version '${OONF_APP_VERSION}'"
+		" is not present in git description '${OONF_APP_GIT}'."
+		" Please re-run 'cmake ..' to update build files")
+ENDIF (NOT FOUND_VERSION)
 
 # create builddata file
 configure_file (${SRC} ${DST})
