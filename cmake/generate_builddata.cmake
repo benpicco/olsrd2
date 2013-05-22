ccd9a5930e3b37f21337137ddc38ae0cf0fc497d
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2013-05-22 07:59:28 +0200

    Simplify builddata

--- cmake/generate_builddata.cmake
+++ cmake/generate_builddata.cmake
@@ -6,13 +6,10 @@ find_program(found_git git)
 IF(${found_git} STREQUAL "found_git-NOTFOUND" OR NOT EXISTS ${GIT})
     # git executable or repository (.git) is not available
     SET(OONF_SRC_GIT "cannot read git repository")
-    SET(OONF_SRC_CHANGE "")
 ELSE()
     # everything is fine, read commit and diff stat
-    execute_process(COMMAND git describe --always --long --tags 
+    execute_process(COMMAND git describe --always --long --tags --dirty
         OUTPUT_VARIABLE OONF_SRC_GIT OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process(COMMAND git diff --shortstat HEAD ./src ./src-plugins ./cmake 
-        OUTPUT_VARIABLE OONF_SRC_CHANGE OUTPUT_STRIP_TRAILING_WHITESPACE)
 ENDIF()
 
 # create builddata file
